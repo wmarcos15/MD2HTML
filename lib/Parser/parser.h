c8d62d188607be0acaf2f8926f0fba9c651f8dde
wmarcos15 <guillermoml9031@gmail.com>
2024-07-29 22:52:33 +0200

    Add basic PARSER_H file

---
+++ lib/Parser/parser.h
@@ -0,0 +1,14 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 1000000
+
+void parseFile(char* filename);
+
+#endif
